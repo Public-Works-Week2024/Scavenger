1359a5a0a14ab6a0ac44e50910a1b394539a0c67
Rohit Sangha <rsangha@countygp.ab.ca>
2024-05-16 10:00:15 -0600

    all

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ function App() {
     const savedStations = localStorage.getItem('stations');
     return savedStations ? JSON.parse(savedStations) : {
       station1: false,
-      station2: false,
-      station3: false,
-      station4: false,
-      station5: false,
-      station6: false,
-      station32: false,
-      station42: false,
+      // station2: false,
+      // station3: false,
+      // station4: false,
+      // station5: false,
+      // station6: false,
+      // station32: false,
+      // station42: false,
 
      
     };
@@ -45,6 +45,8 @@ function App() {
     const savedUser = localStorage.getItem('user');
     return !!savedUser;
   });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const query = useQuery();
 
@@ -70,10 +72,10 @@ function App() {
   };
 
   const handleSubmit = () => {
+    setIsModalOpen(true);
 
     console.log("Submitting:", user, stations);
     if (Math.random() <= 0.9) {
-console.log('TEST',user.phone)
       fetch('https://studentinfoapi.countygp.ab.ca/api/studentinfo/'+user.phone, {
         method: 'POST',
         mode:'no-cors',
@@ -91,26 +93,31 @@ console.log('TEST',user.phone)
 
     
     console.log("Submitting:", user, stations);
-    alert('Submission successful!');
+    setIsModalOpen(true);
 
     localStorage.removeItem('user');
     localStorage.removeItem('stations');
     setUser({ phone: '' });
     setStations({
       station1: false,
-      station2: false,
-      station3: false,
-      station4: false,
-      station5: false,
-      station6: false,
-      station32: false,
-      station42: false,
+      // station2: false,
+      // station3: false,
+      // station4: false,
+      // station5: false,
+      // station6: false,
+      // station32: false,
+      // station42: false,
       
       
     });
 
     setIsRegistered(false);
   };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
 
   const style = {
     color: 'red',
@@ -121,13 +128,12 @@ console.log('TEST',user.phone)
       <div className="image-container top-image-container">
         <img src={topImage} alt="Top Image" className="top-image" />
       </div>
-  
+
       <button className="welcome-button">Welcome</button>
-  
+
       <div className="image-container bottom-image-container">
         <img src={bottomImage} alt="Bottom Image" className="bottom-image" />
         <div className="content-overlay">
-  
           {!isRegistered ? (
             <div className="form-container">
               <h2>Public Works Week Scavenger Hunt Registration</h2>
@@ -140,7 +146,7 @@ console.log('TEST',user.phone)
                 onChange={handleInputChange}
                 required
               />
-               <button type="button" className="register-button" onClick={() => {
+              <button type="button" className="register-button" onClick={() => {
                 if (user.phone) {
                   localStorage.setItem('user', JSON.stringify(user));
                   setIsRegistered(true);
@@ -149,7 +155,6 @@ console.log('TEST',user.phone)
                 Register
               </button>
               <text>Protection of Privacy - Personal information is collected in accordance with Section 33(c) of the Alberta Freedom of Information and Protection of Privacy Act (the FOIP Act) and will be protected under Part 2 of that Act. <text style={style}>Your phone number will only be used for the purpose of entering it into a draw and contacting the winner of the Dinosaur Museum Scavenger Hunt Activity. All phone numbers collected will be deleted following the closure of the contest</text>. Should you require further information about collection, use and disclosure of personal information, please contact the FOIP Coordinator at foip@countygp.ab.ca or call 780-532-9722. </text>
-             
             </div>
           ) : (
             <div className="stations-container">
@@ -162,22 +167,53 @@ console.log('TEST',user.phone)
                 ))}
               </ul>
             </div>
-            
           )}
         </div>
         {!isRegistered ? (<span></span>) : (
-        <button 
-          disabled={!Object.values(stations).every(Boolean)} 
-          onClick={handleSubmit} 
-          className="submit-button"
-        >
-          Submit
-        </button>
+          <button
+            disabled={!Object.values(stations).every(Boolean)}
+            onClick={handleSubmit}
+            className="submit-button"
+          >
+            Submit
+          </button>
         )}
-        
+
+        {isModalOpen && (
+          <div>
+            <div id="background-wrap">
+    <div class="x1">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x2">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x3">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x4">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x5">
+        <div class="cloud"></div>
+    </div>
+</div>
+          <div id="myModal" className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>&times;</span>
+              <p>Awesome! Scavenger Hunt Completed</p>
+            </div>
+          </div>
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
 
 export default App;
